e09fb5a43b5cc64a2b257f48b00a62a6bcc81157
Tomiyama <10380r@gmail.com>
2019-06-10 03:03:38 +0900

    test

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from linebot.exceptions import InvalidSignatureError
 from linebot.models import MessageEvent, TextMessage, TextSendMessage
 import calc
 import os
+import tumblpy
 
 
 app = Flask(__name__)
